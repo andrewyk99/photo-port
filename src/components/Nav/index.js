4cbbbdcbd5588fcf094cea37ae18a90da007ce99
andrewyk99 <andrewyk99@hotmail.com>
2022-06-15 11:13:17 -0600

    created separate page for contact page and conditional rendering

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,8 +5,11 @@ function Nav(props) {
     const {
         categories = [],
         setCurrentCategory,
-        currentCategory
+        currentCategory,
+        contactSelected,
+        setContactSelected
     } = props;
+
     useEffect(() => {
         document.title = capitalizeFirstLetter(currentCategory.name);
     }, [currentCategory]);
@@ -23,18 +26,18 @@ function Nav(props) {
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a data-testid="about" href="#about" onClick={() => handleClick()}>
-                            About Me
-                        </a>
+                        <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>About Me</a>
                     </li>
-                    <li className="mx-2">
-                        <span onClick={() => handleClick()}>
-                            Contact
-                        </span>
+                    <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+                        <span onClick={() => setContactSelected(true)}>Contact</span>
                     </li>
                     {categories.map((category) => (
                         <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
-                            <span onClick={() => { setCurrentCategory(category) }}>
+                            <span onClick={() => { 
+                                setCurrentCategory(category);
+                                setContactSelected(false);
+                                }}
+                            >
                                 {capitalizeFirstLetter(category.name)}
                             </span>
                         </li>
